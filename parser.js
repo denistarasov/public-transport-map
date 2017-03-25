ba36294dabe34ade0b5bd18490b1184bd1ed7d24
Denis Tarasov <denistarasov@users.noreply.github.com>
2017-03-25 22:38:18 +0300

    Add parser and visualization

---
+++ parser.js
@@ -0,0 +1,80 @@
+const request = require('request');
+const fs = require('fs');
+const path = require('path');
+
+// IMPORTANT NOTE:
+// before execution, there should be old files with data
+// ('rawdata' and 'routes' catalogs)
+// in order to replace them with new ones
+
+// collect all routes numbers
+// also, we collect some extra unnecessary info
+const collect_routes = (transport_type, filename) => {
+    const url = `http://transport.orgp.spb.ru/Portal/transport/routes/list`;
+    request.post({
+        url,
+        form: {
+            sEcho: '1',
+            iColumns: '11',
+            sColumns: 'id,transportType,routeNumber,name,urban,poiStart,poiFinish,cost,forDisabled,scheduleLinkColumn,mapLinkColumn',
+            iDisplayStart: '0',
+            iDisplayLength: '1000',
+            sNames: 'id,transportType,routeNumber,name,urban,poiStart,poiFinish,cost,forDisabled,scheduleLinkColumn,mapLinkColumn',
+            iSortingCols: '1',
+            iSortCol_0: '2',
+            sSortDir_0: 'asc',
+            'transport-type':  transport_type,
+        },
+    }, (err, res, body) => {
+        // console.log(path.join(__dirname, filename));
+        fs.writeFileSync(path.join(__dirname, filename), body);
+        console.log(`Successfully parsed data to \"${filename}\"`);
+    });
+}
+
+const collect_routes_numbers = (/*num_json, */num_arr) => {
+    combined_routes = [
+        fs.readFileSync(__dirname + '/rawdata/bus_routes.json'),
+        fs.readFileSync(__dirname + '/rawdata/trolley_routes.json'),
+        fs.readFileSync(__dirname + '/rawdata/tram_routes.json'),
+        fs.readFileSync(__dirname + '/rawdata/aquabus_routes.json'),
+    ];
+    combined_routes.forEach(routesJson => {
+        const routes = JSON.parse(routesJson);
+        routes['aaData'].forEach(route => {num_arr.push(route[0])});
+    });
+    num_arr.sort(function (a, b) { return a - b; });
+    console.log("Number of routes:", num_arr.length);
+}
+
+count = 0;
+const parse_route = (num_arr, count) => {
+    if (count >= num_arr.length)
+        return;
+    const id = num_arr[count];
+    console.log(`Currently parsing route #${id}`);
+    const url = `http://transport.orgp.spb.ru/Portal/transport/map/poi?ROUTE=${id}&REQUEST=GetFeature&_=1488372392174`;
+    request({
+        url,
+    }, (err, res, body) => {
+        fs.writeFileSync(path.join(__dirname, `/routes/${id}.json`), body);
+    });
+    ++count;
+
+    setTimeout(function() {
+        parse_route(num_arr, count);
+    }, wait_time);
+};
+
+collect_routes(0, 'rawdata/bus_routes.json');
+collect_routes(1, 'rawdata/trolley_routes.json');
+collect_routes(2, 'rawdata/tram_routes.json');
+collect_routes(46, 'rawdata/aquabus_routes.json');
+var num_json;
+var num_arr = [];
+collect_routes_numbers(num_arr);
+
+const wait_time = 3000; // as we don't want to DDOS server
+setTimeout(function() {
+    parse_route(num_arr, 0);
+}, wait_time);
---
+++ transform.js
@@ -0,0 +1,55 @@
+const request = require('request');
+const fs = require('fs');
+const path = require('path');
+const sphericalmercator = require('sphericalmercator');
+
+// open the whole catalog
+const filenames = fs.readdirSync(__dirname + `/routes`);
+
+// now read all route in one json array
+const routeFiles = [];
+filenames.forEach(filename => {
+    console.log(filename);
+    /*routeFiles.push({
+        filename: fs.readFileSync(__dirname + `/routes/${filename}`)
+    });*/
+    routeFiles.push(fs.readFileSync(__dirname + `/routes/${filename}`));
+});
+
+// json string containing all routes and set of stops
+const result = {
+    routes: {},
+    stops: {},
+};
+
+var merc = new sphericalmercator({
+    size: 256
+});
+
+var routeId = 0;
+routeFiles.forEach(routeStr => {
+    const route = JSON.parse(routeStr);
+    route['features'].forEach(stop => {
+        var coords = merc.inverse([stop.geometry.coordinates[0],stop.geometry.coordinates[1]]);
+        result.stops[stop.id] = {
+            lat: coords[1],
+            long: coords[0],
+            title: stop.properties.name,
+        };
+    });
+
+    ++routeId;
+    result.routes[routeId] = {
+        trips: {
+            A: {
+                stops: route['features'].map(stop => stop.id),
+                shape: route['features'].map(stop => {
+                var coords = merc.inverse([stop.geometry.coordinates[0],stop.geometry.coordinates[1]]);
+                return [coords[1], coords[0]];
+                }),
+            }
+        },
+    };
+});
+const resJson = JSON.stringify(result, null, '  ');
+fs.writeFileSync('data.json', resJson);
