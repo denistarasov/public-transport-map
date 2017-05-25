b81fd877901773f37ba44b572beaf1e71a521ecc
Denis Tarasov <denistarasov@users.noreply.github.com>
2017-05-25 23:09:02 +0300

    Add fixed transform.js and data.json

--- transform.js
+++ transform.js
@@ -5,34 +5,17 @@ const sphericalmercator = require('sphericalmercator');
 
 // open the whole catalog
 const filenames = fs.readdirSync(__dirname + `/routes`);
-filenames.sort(); // for easier finding not matching ${id} and ${id}_shape
 
 // now read all route in one json array
 const routeFiles = [];
 const shapeFiles = [];
-// check = 0;
 filenames.forEach(filename => {
-    /*routeFiles.push({
-        filename: fs.readFileSync(__dirname + `/routes/${filename}`)
-    });*/
+    // console.log(filename);
     if (!filename.match(/(.*)_shape.json/)) { // if not shapes but routes
         routeFiles.push(fs.readFileSync(__dirname + `/routes/${filename}`));
-        // checking missing routes
-        // console.log("route:", filename);
-        // ++check;
-        // if (check < 0 || check > 1) {
-        //     console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-        //     check = 1;
-        // }
-    } else { // if filename contains a shape
-        shapeFiles.push(fs.readFileSync(__dirname + `/routes/${filename}`));
-        // checking missing shapes
-        // console.log("shape:", filename);
-        // --check;
-        // if (check < 0 || check > 1) {
-        //     console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-        //     check = 0;
-        // }
+        // and push ${id}_shape.json
+        const id = filename.split('.')[0];
+        shapeFiles.push(fs.readFileSync(__dirname + `/routes/${id}_shape.json`));
     }
 });
 
@@ -46,8 +29,32 @@ var merc = new sphericalmercator({
     size: 256
 });
 
-for (var i = 0; i < routeFiles.length; ++i) {
-    const route = JSON.parse(routeFiles[i]);
+var shapeId = 0;
+shapes = [];
+shapeFiles.forEach(routeStr => {
+    // console.log(routeStr);
+    const shapeFromJson = JSON.parse(routeStr);
+
+    ++shapeId;
+    // shapes[shapeId] = 
+    //     shapeFromJson['features'].map(stop => {
+    //         var coords = merc.inverse([stop.geometry.coordinates[0],stop.geometry.coordinates[1]]);
+    //         return [coords[1], coords[0]];
+    //     });
+    // var shape = [];
+    shapes[shapeId] = [];
+    shapeFromJson['features'].forEach(item => {
+        item['geometry']['coordinates'].forEach(elem => {
+            shapes[shapeId].push(elem);
+        });
+    });
+});
+
+
+var routeId = 0;
+routeFiles.forEach(routeStr => {
+    // console.log(routeStr);
+    const route = JSON.parse(routeStr);
     route['features'].forEach(stop => {
         var coords = merc.inverse([stop.geometry.coordinates[0],stop.geometry.coordinates[1]]);
         result.stops[stop.id] = {
@@ -57,57 +64,30 @@ for (var i = 0; i < routeFiles.length; ++i) {
         };
     });
 
-    const shapes = JSON.parse(shapeFiles[i]);
-    // parse shapes to one shape consisting of all points forming the shape
-    var shape = [];
-    shapes['features'].forEach(item => {
-        item['geometry']['coordinates'].forEach(elem => {
-            shape.push(elem);
-        });
-    });
-    // console.log(shape);
-    result.routes[i] = {
+    ++routeId;
+    result.routes[routeId] = {
         trips: {
             A: {
                 stops: route['features'].map(stop => stop.id),
-                shape: shape.map(shapePoint => {
-                    // sh
+                // shape: route['features'].map(stop => {
+                // var coords = merc.inverse([stop.geometry.coordinates[0],stop.geometry.coordinates[1]]);
+                // return [coords[1], coords[0]];
+                // }),
+                shape: shapes[routeId].map(shapePoint => {
                     // shapePoints = shape['geometry']['coordinates'];
                     // var coords = merc.inverse([stop.geometry.coordinates[0],stop.geometry.coordinates[1]]);
                     //     return [coords[1], coords[0]];
                     var coords = merc.inverse([shapePoint[0],shapePoint[1]]);
                     // console.log([coords[1], coords[0]]);
-                    return [coords[1], coords[0]];
+                    return {
+                        lat: coords[1],
+                        lon: coords[0]
+                    };
                 }),
+                // shape: shapes[routeId]
             }
         },
     };
-}
-
-// var routeId = 0;
-// routeFiles.forEach(routeStr => {
-//     const route = JSON.parse(routeStr);
-//     route['features'].forEach(stop => {
-//         var coords = merc.inverse([stop.geometry.coordinates[0],stop.geometry.coordinates[1]]);
-//         result.stops[stop.id] = {
-//             lat: coords[1],
-//             long: coords[0],
-//             title: stop.properties.name,
-//         };
-//     });
-
-//     ++routeId;
-//     result.routes[routeId] = {
-//         trips: {
-//             A: {
-//                 stops: route['features'].map(stop => stop.id),
-//                 shape: route['features'].map(stop => {
-//                     var coords = merc.inverse([stop.geometry.coordinates[0],stop.geometry.coordinates[1]]);
-//                     return [coords[1], coords[0]];
-//                 }),
-//             }
-//         },
-//     };
-// });
+});
 const resJson = JSON.stringify(result, null, '  ');
 fs.writeFileSync('data.json', resJson);
