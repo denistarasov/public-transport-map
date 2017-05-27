c881a2c1ab8d8984ed63c50172423e77d7fa89e1
Denis Tarasov <denistarasov@users.noreply.github.com>
2017-05-27 12:35:25 +0300

    Add different stop styles

--- leaflet.js
+++ leaflet.js
@@ -5,18 +5,30 @@ map.addLayer(
 );
 
 const polylines = [];
+const markers = [];
+const stops = [];
 
 fetch('data.json')
     .then(r => r.json())
     .then(data => {
         Object.values(data.stops).forEach(s => {
-            L.circleMarker([s.lat, s.long], {
+            stops.push(s);
+            markers.push(L.circleMarker([s.lat, s.long], {
                 stroke: false,
                 color: '#000',
                 fillOpacity: 0.8,
                 radius: 5,
-            }).addTo(map);
+            }).addTo(map));
         });
+        // Object.values(data.stops).forEach(s => {
+        //     markers.push(L.marker([s.lat, s.long], {
+        //         icon: square_stop,
+        //         // stroke: false,
+        //         // color: '#000',
+        //         // fillOpacity: 0.8,
+        //         // radius: 5,
+        //     }).addTo(map));
+        // });
 
         Object.values(data.routes).forEach(r => {
             Object.values(r.trips).forEach(t => {
@@ -34,4 +46,42 @@ function changeColor() {
             color: color
         });
     });
+}
+
+function changeStop() {
+    markers.forEach(m => {
+        map.removeLayer(m);
+    });
+    if (document.getElementById("option1").checked) {
+        stops.forEach(s => {
+            markers.push(L.circleMarker([s.lat, s.long], {
+                stroke: false,
+                color: '#000',
+                fillOpacity: 0.8,
+                radius: 5,
+            }).addTo(map).bringToBack());
+        });
+    }
+    else if (document.getElementById("option2").checked) {
+        var myIcon = new L.divIcon({
+            className: 'round_with_void_stop',
+            iconSize: 5
+        });
+        stops.forEach(s => {
+            markers.push(L.marker([s.lat, s.long], {
+                icon: myIcon
+            }).addTo(map));
+        });
+    }
+    else if (document.getElementById("option3").checked) {
+        var myIcon = new L.divIcon({
+            className: 'square_stop',
+            iconSize: 5
+        });
+        stops.forEach(s => {
+            markers.push(L.marker([s.lat, s.long], {
+                icon: myIcon
+            }).addTo(map));
+        });
+    }
 }
