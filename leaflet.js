28b061d76d7517be872f4f013de8c558f39ff197
Denis Tarasov <denistarasov@users.noreply.github.com>
2017-06-01 15:12:01 +0300

    Add one route coloring

--- leaflet.js
+++ leaflet.js
@@ -12,6 +12,45 @@ const polylines = [];
 const markers = [];
 const stops = [];
 
+var initialStyle = {
+    color: '#2d9149',
+    weight: 3,
+    renderer: myRenderer
+};
+var hoverStyle = {
+    color: "#333",
+    weight: 7,
+    renderer: myRenderer
+};
+var clickStyle = {
+    color: "#111",
+    weight: 10,
+    renderer: myRenderer
+};
+
+function fOnMouseover() {
+    this.setStyle(hoverStyle);
+    this.bringToFront();
+}
+
+function fOnMouseout() {
+    this.setStyle(initialStyle);
+}
+
+function fOnClick() {
+    // wasClicked[i] = true;
+    chosenPolyline = this;
+    this.setStyle(clickStyle);
+    this.bringToFront();
+    polylines.forEach(p => {
+        p.off("mouseout");
+        p.off("mouseover");
+        p.off("click");
+    });
+}
+
+var chosenPolyline = L.polyline([[0, 0]]);
+
 fetch('data.json')
     .then(r => r.json())
     .then(data => {
@@ -34,28 +73,19 @@ fetch('data.json')
         //         // radius: 5,
         //     }).addTo(map));
         // });
-        var initialStyle = {
-            color: '#2d9149',
-            weight: 3,
-            renderer: myRenderer
-        };
-        var hoverStyle = {
-            color: "#112233",
-            weight: 9,
-            renderer: myRenderer
-        }
 
+        // wasClicked = [];
+        // alert("length is " + wasClicked.length);
         Object.values(data.routes).forEach(r => {
+            // var counter = 0;
             Object.values(r.trips).forEach(t => {
                 polylines.push(L.polyline(t.shape, initialStyle)
-                .on("mouseover", function () {
-                    this.setStyle(hoverStyle);
-                    this.bringToFront();
-                })
-                .on("mouseout", function () {
-                    this.setStyle(initialStyle);
-                })
+                .on("mouseover", fOnMouseover)
+                .on("mouseout", fOnMouseout)
+                .on("click", fOnClick)
                 .addTo(map));
+                // wasClicked.push(false);
+                // ++counter;
             });
         });
     });
@@ -64,10 +94,19 @@ fetch('data.json')
 // when 'change color' button pressed
 function changeColor() {
     var color = '#' + document.getElementById("color-choice").value;
+    // polylines.forEach(p => {
+    //     p.setStyle({
+    //         color: color
+    //     });
+    // });
+    chosenPolyline.setStyle(initialStyle);
+    chosenPolyline.setStyle({
+        color: color
+    });
     polylines.forEach(p => {
-        p.setStyle({
-            color: color
-        });
+        p.on("mouseover", fOnMouseover);
+        p.on("mouseout", fOnMouseout);
+        p.on("click", fOnClick);
     });
 }
 
